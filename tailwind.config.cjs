bac8ac8610cdd825ca0b9ab86f914d249b8351d4
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-23 01:27:59 +0100

    install tailwind plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,18 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-nocheck
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require("tailwindcss-elevation")(["responsive"]),
+    require("tailwindcss-no-scrollbar"),
+  ],
 };
 
 module.exports = config;
