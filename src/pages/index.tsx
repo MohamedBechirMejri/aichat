9af0bef48b90fdb4924eab951e908479dab89d47
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-26 06:18:07 +0100

    submit on ctrl+enter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -176,6 +176,9 @@ const Home: NextPage = () => {
               name="prompt"
               placeholder="Ask me Anything..."
               value={value}
+              onKeyDown={(e) => {
+                if (e.ctrlKey && e.key === "Enter") void handleSubmit(e);
+              }}
               onChange={(e) => setValue(e.target.value)}
               className="flex w-full resize-none items-center justify-center rounded-[1.5rem] bg-white bg-opacity-60 p-3 px-12 outline-none elevation-1 placeholder:text-[#898989]"
               maxLength={200}
@@ -202,7 +205,7 @@ const Home: NextPage = () => {
                 className="absolute right-4 top-4 w-6 fill-blue-700"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048">
-                  <path d="M64 1920q-28 0-46-18t-18-47q0-7 2-17l189-658q5-17 19-30t32-16l878-139q14-2 22-11t8-24q0-14-8-23t-22-12L242 786q-18-3-32-16t-19-30L2 82Q0 72 0 65q0-28 18-46T64 0q15 0 27 6l1920 896q17 8 27 23t10 35q0 19-10 34t-27 24L91 1914q-12 6-27 6z"></path>
+                  <path d="M64 1920q-28 0-46-18t-18-47q0-7 2-17l189-658q5-17 19-30t32-16l878-139q14-2 22-11t8-24q0-14-8-23t-22-12L242 786q-18-3-32-16t-19-30L2 82Q0 72 0 65q0-28 18-46T64 0q15 0 27 6l1920 896q17 8 27 23t10 35q0 19-10 34t-27 24L91 1914q-12 6-27 6z" />
                 </svg>
               </motion.button>
             )}
