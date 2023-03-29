c85a4c86d459d6ac94d54d4ddf383a8a09a7c96b
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-03-29 11:31:02 +0100

    add models options

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,13 +1,36 @@
+import { motion } from "framer-motion";
+
 const Settings = () => {
   return (
-    <div className="gradientbg fixed top-0 left-0 grid h-full w-[100svw] grid-cols-1 grid-rows-[auto,1fr,auto] place-items-center gap-8 p-4 pt-8">
-      <div className="flex w-full max-w-[34rem] flex-col gap-4">
-        <label htmlFor="api key">OpenAI API Key</label>
-        <input
-          type="text"
-          id="api key"
-          className="h-12 rounded-2xl p-2 outline-none elevation-2"
-        />
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="gradientbg fixed top-0 left-0 grid h-full w-[100svw] grid-cols-1 grid-rows-[auto,1fr,auto] place-items-center gap-8 p-4 pt-8"
+    >
+      <div className="grid w-full max-w-[34rem] grid-cols-2 gap-4">
+        <div className="flex flex-col gap-4">
+          <label htmlFor="api key">OpenAI API Key</label>
+          <input
+            type="text"
+            id="api key"
+            className="h-12 rounded-2xl p-2 outline-none elevation-2"
+          />
+        </div>
+        <div className="flex flex-col gap-4">
+          <label htmlFor="model">Model</label>
+          <select
+            id="model"
+            className="h-12 rounded-2xl p-2 outline-none elevation-2"
+          >
+            <option value="gpt-3.5-turbo">chatGPT (gpt-3.5-turbo)</option>
+            <option value="davinci">GPT3 (Davinci)</option>
+            <option value="gpt4" disabled>
+              GPT4 (soon)
+            </option>
+            <option value="curie">VisionVortexes </option>
+          </select>
+        </div>
       </div>
       <div className="flex h-full w-full max-w-[34rem] flex-col gap-4">
         <label htmlFor="system">System Message</label>
@@ -41,7 +64,7 @@ const Settings = () => {
           </a>
         </p>
       </footer>
-    </div>
+    </motion.div>
   );
 };
 
